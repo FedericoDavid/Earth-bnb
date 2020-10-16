162d687a0e1b66c0839ab8c6491618b8a511d233
FedericoDavid <fedenri98@gmail.com>
2020-10-16 00:31:32 -0300

    Search bar & first part of the banner, without search dates

---
+++ src/Banner.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import "./Banner.css" 
+import { Button } from "@material-ui/core";
+
+function Banner() {
+    
+    const [showSearch, setShowSearch] = useState(false);
+   
+    return (
+        <div className="banner">
+           <div className="banner__search">
+               {showSearch && <h1>SHOW DATE PICKER</h1>}
+               <Button onClick={() => setShowSearch(!showSearch)}  
+                       className="banner__searchButton" 
+                       variant="outlined">Search Dates</Button>
+           </div>
+           <div className="banner__info">
+               <h1>Go Near</h1>
+               <h5>Settle in somewhere new. Discover nearby stays to live, 
+                   work, or just relax.</h5>
+               <Button variant="outlined">Explore Nearby</Button>   
+           </div>
+        </div>
+    )
+}
+
+export default Banner
